cd0b000266bfeac0ffdfc07a4cca17ce3569cf94
stoffelrenan <67083588+stoffelrenan@users.noreply.github.com>
2022-05-24 13:22:59 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
 
+st.write("My First Streamlit Web App")
+
+def get_data(currency):
+    data = yf.download(tickers=currency+'-USD', period = '5y', interval = '1d')
+    data.name=currency
+    return data
+  
+option = st.selectbox(
+'How would you like to be contacted?',
+('BTC','LTC','LUNA1'))
+
+if st.form_submit_button(label="Choose coin", help=None, on_click=None, args=None, kwargs=None):
+  df = get_data(option)
